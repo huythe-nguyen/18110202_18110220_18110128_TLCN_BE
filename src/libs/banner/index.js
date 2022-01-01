3f30af460ca47031a213b25b3ead8466327b3043
huythe-nguyen <nguyenhuythe1997@gmail.com>
2022-01-01 19:21:19 +0700

    new

--- src/libs/banner/index.js
+++ src/libs/banner/index.js
@@ -12,7 +12,6 @@ module.exports = (log) => {
         log.info(`Version      : ${env.app.version}`)
         log.info(``)
         log.info(`API Info     : ${route()}${env.app.routePrefix}`)
-<<<<<<< HEAD
         // if (env.swagger.enabled) {
         //     log.info(`Swagger      : ${route()}${env.swagger.route}`)
         // }
@@ -21,16 +20,6 @@ module.exports = (log) => {
         // }
         // log.info('-------------------------------------------------------')
         // log.info('')
-=======
-//         if (env.swagger.enabled) {
-//             log.info(`Swagger      : ${route()}${env.swagger.route}`)
-//         }
-//         if (env.monitor.enabled) {
-//             log.info(`Monitor      : ${route()}${env.monitor.route}`)
-//         }
-//         log.info('-------------------------------------------------------')
-//         log.info('')
->>>>>>> 84e33ce1cc71ad63c698b92d873e4da0a5302da5
     } else {
         log.info(`Application is up and running.`)
     }
